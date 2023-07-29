40f6532b05b90076ccf4b31db5ba83b0858797b5
Neto test <munkey@list.ru>
2023-07-29 12:03:25 +0300

    Origin master

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import junit.framework.TestCase;
+
+public class CashbackHackServiceTest extends TestCase {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.Test
+    public void testRemainWiWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+}
